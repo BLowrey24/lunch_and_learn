fd094b1e72296457723ba950353e34fe33ab5806
Boston Lowrey <bostonlowrey@gmail.com>
2023-08-05 17:39:09 -0400

    feat: add functionality for recipes controller to get a country from the country facade

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::RecipesController < ApplicationController
   def index
-    @recipes = RecipeFacade.new.get_recipes(params[:country])
-    render json: RecipeSerializer.new(@recipes)
+    country = CountryFacade.new.get_country(params[:country])
+     recipes = RecipeFacade.new.get_recipes(country)
+    render json: RecipeSerializer.new(recipes)
   end
 end

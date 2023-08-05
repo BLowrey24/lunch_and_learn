bce5f655cf2da02a1070f97f4a8a8e01abb07e6d
Boston Lowrey <bostonlowrey@gmail.com>
2023-08-05 17:44:55 -0400

    feat: setup vcr on tests

--- spec/requests/recipe_request_spec.rb
+++ spec/requests/recipe_request_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "get recipes", type: :request do
   context "happy path" do
-    it "returns a list of recipes based on the country passed" do
+    it "returns a list of recipes based on the country passed", :vcr => { :record => :new_episodes } do
       get "/api/v1/recipes?country=spain"
       expect(response).to be_successful
       recipes = JSON.parse(response.body, symbolize_names: true)
@@ -27,7 +27,7 @@ RSpec.describe "get recipes", type: :request do
       expect(recipes[:data].first[:attributes][:image]).to be_a String
     end
 
-    it "returns a list of recipes for a random country when country is not specified" do
+    it "returns a list of recipes for a random country when country is not specified", :vcr => { :record => :new_episodes } do
       get "/api/v1/recipes"
       expect(response).to be_successful
       recipes = JSON.parse(response.body, symbolize_names: true)

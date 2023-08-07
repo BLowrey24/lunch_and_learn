f6728416285d82bccb43e35604b85a2da98b376f
Boston Lowrey <bostonlowrey@gmail.com>
2023-08-07 13:15:54 -0400

    refactor air_quality_facade spec

--- spec/facades/air_quality_facade_spec.rb
+++ spec/facades/air_quality_facade_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.describe AirQualityFacade,type: :facade do
   describe ".air_quality" do
     it "fetches and processes air quality data", :vcr => { :record => :new_episodes } do
-      air_quality_data = AirQualityFacade.new.air_quality("Abuja")
+      air_quality_data = AirQualityFacade.new.air_quality("Cuba")
 
-      expect(air_quality_data.city).to eq("Abuja")
+      expect(air_quality_data.city).to eq("Cuba")
       expect(air_quality_data.aqi).to be_a Integer
       expect(air_quality_data.pm25_concentration).to be_a Float
       expect(air_quality_data.co_concentration).to be_a Float

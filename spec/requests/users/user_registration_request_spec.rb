955453e592d32d74d985d200fe8b257d7c09ab77
Boston Lowrey <bostonlowrey@gmail.com>
2023-08-07 17:29:11 -0400

    test: write test for user registration

--- spec/requests/users/user_registration_request_spec.rb
+++ spec/requests/users/user_registration_request_spec.rb
@@ -1,15 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Users API", type: :request do
-  xdescribe 'POST #create' do
+  describe 'POST #create' do
     it 'creates a new User' do
-      expect {
-        post "/api/v1/users", params: { user: valid_attributes }
-      }.to change(User, :count).by(1)
 
-      post "/api/v1/users", params: { user: valid_attributes }
+      valid_params = {
+        "name": "Odell",
+        "email": "goodboy@ruffruff.com",
+        "password": "treats4lyf",
+        "password_confirmation": "treats4lyf"
+      }
+
+      post "/api/v1/users", params: { user: valid_params }
+
       expect(response).to have_http_status(:created)
-      expect(JSON.parse(response.body)).to include('api_key')
+      user = JSON.parse(response.body, symbolize_names: true)
+
+      expect(user).to have_key(:data)
+      expect(user[:data]).to have_key(:type)
+      expect(user[:data][:type]).to eq("user")
+      expect(user[:data]).to have_key(:id)
+      expect(user[:data][:id]).to be_a Integer
+      expect(user[:data]).to have_key(:attributes)
+      expect(user[:data][:attributes]).to have_key(:name)
+      expect(user[:data][:attributes][:name]).to be_a String
+      expect(user[:data][:attributes]).to have_key(:email)
+      expect(user[:data][:attributes][:email]).to be_a String
+      expect(user[:data][:attributes]).to have_key(:api_key)
+      expect(user[:data][:attributes][:api_key]).to be_a String
     end
   end
 end

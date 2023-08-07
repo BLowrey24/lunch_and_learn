8de5a576d7d2263269f7affc28c86630e8130f5a
Boston Lowrey <bostonlowrey@gmail.com>
2023-08-07 12:49:55 -0400

    feat: finish first test for getting air_quality for a specific capital city

--- spec/requests/air_quality/get_air_quality_for_a_capital_city_request_spec.rb
+++ spec/requests/air_quality/get_air_quality_for_a_capital_city_request_spec.rb
@@ -5,6 +5,27 @@ RSpec.describe "Air Quality API", type: :request do
     it "get air quality for a capital's city", :vcr => { :record => :new_episodes } do
       country = "Nigeria"
       get "/api/v1/air_quality?country=#{country}"
+      expect(response).to be_successful
+      parsed_body = JSON.parse(response.body, symbolize_names: true)
+      #  require 'pry'; binding.pry
+
+      expect(parsed_body).to have_key(:data)
+      expect(parsed_body[:data]).to have_key(:id)
+      expect(parsed_body[:data][:id]).to be(nil)
+      expect(parsed_body[:data]).to have_key(:type)
+      expect(parsed_body[:data][:type]).to eq("air_quality")
+      expect(parsed_body[:data]).to have_key(:city)
+      expect(parsed_body[:data][:city]).to eq("Abuja")
+      expect(parsed_body[:data]).to have_key(:attributes)
+      expect(parsed_body[:data][:attributes]).to be_a Hash
+
+
+      expect(parsed_body[:data][:attributes]).to have_key(:aqi)
+      expect(parsed_body[:data][:attributes][:aqi]).to be_a Integer
+      expect(parsed_body[:data][:attributes]).to have_key(:pm25_concentration)
+      expect(parsed_body[:data][:attributes][:pm25_concentration]).to be_a Float
+      expect(parsed_body[:data][:attributes]).to have_key(:co_concentration)
+      expect(parsed_body[:data][:attributes][:co_concentration]).to be_a Float
     end
   end
 end

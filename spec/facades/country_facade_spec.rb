81b21878e65f3a3055028436b8928b5c14cec9da
Boston Lowrey <bostonlowrey@gmail.com>
2023-08-07 11:15:35 -0400

    test: write tests for country facade method and country service method that gets a capital for a given country

--- spec/facades/country_facade_spec.rb
+++ spec/facades/country_facade_spec.rb
@@ -1,21 +1,28 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe CountryFacade do
-  describe '.get_country' do
-    context 'when country is nil' do
-      it 'returns a random country name', :vcr => { :record => :new_episodes } do
+  describe ".get_country" do
+    context "when country is nil" do
+      it "returns a random country name", :vcr => { :record => :new_episodes } do
         result = CountryFacade.new.get_country(nil)
 
         expect(result).to be_a String
       end
     end
 
-    context 'when country is not nil' do
-      it 'returns the given country name', :vcr => { :record => :new_episodes } do
-        result = CountryFacade.new.get_country('Spain')
+    context "when country is not nil" do
+      it "returns the given country name", :vcr => { :record => :new_episodes } do
+        result = CountryFacade.new.get_country("Spain")
 
-        expect(result).to eq('Spain')
+        expect(result).to eq("Spain")
       end
+    end
+  end
+
+  describe ".get_country_capital", vcr: { record: :new_episodes } do
+    it "returns the capital of a specific country" do
+      result = CountryFacade.new.get_country_capital("Mexico")
+      expect(result).to eq("Mexico City")
     end
   end
 end
--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -1,15 +1,24 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe CountryService do
-  it '.all_countries', :vcr => { :record => :new_episodes } do
-    countries = CountryService.new.all_countries
+  describe ".all_countries", :vcr => { :record => :new_episodes } do
+    it "returns an array of countries" do
+      countries = CountryService.new.all_countries
 
-    expect(countries).to be_a Array
+      expect(countries).to be_a Array
 
-    countries.each do |country|
+      countries.each do |country|
 
-      expect(country).to have_key :name
-      expect(country[:name]).to have_key :common
+        expect(country).to have_key :name
+        expect(country[:name]).to have_key :common
+      end
+    end
+  end
+
+  describe ".get_country_capital", vcr: { record: :new_episodes } do
+    it "returns the capital of a specific country" do
+      capital = CountryService.new.get_country_capital("Mexico")
+      expect(capital).to eq("Mexico City")
     end
   end
 end

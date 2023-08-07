cc7a49728754a89d3f0e2a7429ac192071f3dad7
Boston Lowrey <bostonlowrey@gmail.com>
2023-08-07 12:51:24 -0400

    update: get air quality facade method to work and return the correct data needed

--- app/facades/air_quality_facade.rb
+++ app/facades/air_quality_facade.rb
@@ -4,7 +4,7 @@ class AirQualityFacade
   end
 
   def air_quality(city)
-    air_quality_data = service.get_air_quality(city).body
+    air_quality_data = service.get_air_quality(city)
     AirQualityData.new(air_quality_data, city)
   end
 end

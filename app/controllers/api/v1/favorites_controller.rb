1a60ef41659f8474532c36e22cecd0b7bda21cbc
Boston Lowrey <bostonlowrey@gmail.com>
2023-08-08 16:07:09 -0400

    feat: create functionality for favorites index action

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::FavoritesController < ApplicationController
+  def index
+    user = User.find_by(api_key: params[:api_key])
+    if user.present?
+      favorites = user.favorites
+      render json: FavoriteSerializer.new(favorites), status: :ok
+    else
+      render json: { error: "Invalid API key" }, status: 400
+    end
+  end
+
   def create
     user = User.find_by(api_key: params[:api_key])
     if user.present?

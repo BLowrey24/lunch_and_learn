ca5ef9a2cbe2ed78d1c4d7009c68b4096dba4f1a
Boston Lowrey <bostonlowrey@gmail.com>
2023-08-08 13:22:05 -0400

    test: write tests for the generate_api_key method in user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,13 +15,13 @@ RSpec.describe User, type: :model do
 
   describe "generate_api_key" do
     it "generates a unique API key for the user" do
-      user = User.create!(email: "api@example.com", password: "api_password", password_confirmation: "api_password")
+      user = User.create!(email: "baston@example.com", password: "password1324", password_confirmation: "password1324")
       expect(user.api_key).to be_present
     end
 
     it "generates a different API key for different users" do
-      user1 = User.create!(email: "user1@example.com", password: "user1_password", password_confirmation: "user1_password")
-      user2 = User.create!(email: "user2@example.com", password: "user2_password", password_confirmation: "user2_password")
+      user1 = User.create!(email: "baston@example.com", password: "baston-password", password_confirmation: "baston-password")
+      user2 = User.create!(email: "leroy@example.com", password: "leroy-password", password_confirmation: "leroy-password")
 
       expect(user1.api_key).not_to eq(user2.api_key)
     end
